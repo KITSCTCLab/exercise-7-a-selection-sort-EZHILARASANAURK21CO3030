97e749c70d6461847d153f09291692dd497c19ac
EZHILARASANAURK21CO3030 <110016230+EZHILARASANAURK21CO3030@users.noreply.github.com>
2022-10-18 11:58:00 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,24 @@
 class Node:
-  def __init__(self, data):
+  def _from typing import List
+
+def selectionSort(array, size) -> List[int]:
+    for y in range(len(array)):
+        mid = y
+        for j in range(y+1,len(array)):
+            if array[mid] > array[j]:
+                mid = j
+        array[y], array[mid] = array[mid] , array[y]
+    return array
+
+input_data = input()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+print(selectionSort(data, len(data)))
+_init__(self, data):
     self.data = data
     self.next = None
 
